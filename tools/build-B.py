bbce6b56771c93b8455cad78e2af430958d64613
MurphyZhao <d2014zjt@163.com>
2019-02-18 12:50:05 +0800

    更新 ci
    Signed-off-by: MurphyZhao <d2014zjt@163.com>

---
+++ tools/build-B.py
@@ -0,0 +1,19 @@
+#!/user/bin/python
+#coding:utf-8
+
+import os
+import sys
+import time
+import datetime
+import platform
+
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+
+if __name__ == '__main__':
+    print("==== In main function of (%s) ====" % (__file__))
+
+    print sys.argv
+
+    exit(0)
